bbbbeac895c0e3a6fd3a355a3263953042919667
Herbert Joseph <techniquea2z@gmail.com>
2025-05-26 12:55:48 -0400

    fix(symbol): use symbol instead of string

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::API
   def current_user
     if request.headers["Authorization"].present?
       jwt_payload = JWT.decode(request.headers["Authorization"].split(" ").last, Rails.application.credentials.devise_jwt_secret_key!).first
-      User.find_by(jti: jwt_payload["jti"])
+      User.find_by(jti: jwt_payload[:jti])
     else
       throw StandardError.new("Authorization Header is missing.")
     end
