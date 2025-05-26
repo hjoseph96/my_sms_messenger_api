5a16f0839b0a6a07599ecf94eaadb92080461b9b
Herbert Joseph <techniquea2z@gmail.com>
2025-05-26 13:43:24 -0400

    fix(api-url): Set to production uri

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,10 @@ class ApplicationController < ActionController::API
   def current_user
     if request.headers["Authorization"].present?
       jwt_payload = JWT.decode(request.headers["Authorization"].split(" ").last, Rails.application.credentials.devise_jwt_secret_key!).first
-      User.find_by(jti: jwt_payload[:jti])
+
+      token = jwt_payload[:jti] || jwt_payload['jti']
+
+      User.find_by(jti: token)
     else
       throw StandardError.new("Authorization Header is missing.")
     end
