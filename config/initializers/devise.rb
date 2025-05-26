d6970127f7f72d598ae50a138a20274e59d3e61c
Herbert Joseph <techniquea2z@gmail.com>
2025-05-26 19:57:36 -0400

    fix(debug) debug Rails.application.credentials.devise_jwt_secret_key! blankness

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -10,7 +10,7 @@
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
   config.jwt do |jwt|
-    jwt.secret = Rails.application.credentials.devise_jwt_secret_key!
+    jwt.secret = Rails.application.credentials.devise_jwt_secret_key
     jwt.dispatch_requests = [
       [ "POST", %r{^/login$} ]
     ]
